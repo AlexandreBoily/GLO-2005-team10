1da44fe78d358a459be818ed99fc669fe2fdb4c9
Jean-Thomas Baillargeon <31716976+jtbai@users.noreply.github.com>
2019-03-13 16:54:46 -0400

    generalize the theme

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-application = Flask("my_glo4035_application")
+application = Flask("my_application")
 
 @application.route("/", methods=["GET","POST"])
 def index():
 
-    return "Oh yeah"
+    return "Hello World"
 
 if __name__ ==  "__main__":
     application.run(host="0.0.0.0", port=80)
