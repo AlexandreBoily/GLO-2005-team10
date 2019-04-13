724a3ca1d725af4d161e96ffb8829fd0e40f32e1
iamurrDev <antoine.iamurri@gmail.com>
2019-04-13 16:37:21 -0400

    implemented basic navigation for each page

--- app/app.py
+++ app/app.py
@@ -4,13 +4,42 @@ from flask import Flask, render_template
 from MySQLdb import MySQLRepository
 application = Flask(__name__)
 
+
+db = MySQLRepository()
+
+
 @application.route("/", methods=["GET","POST"])
 def index():
+    return render_template('index.html')
 
-    db = MySQLRepository()
+
+@application.route("/games", methods=["GET"])
+def games():
+    global db
     games = db.getGames()
-    return render_template('index.html', games=games)
+    return render_template('list.html', items=games, pageName='Games')
 
-    # return "Hello World test"
+
+@application.route("/leagues", methods=["GET"])
+def leagues():
+    global db
+    games = db.getGames()
+    return render_template('list.html', items=games, pageName='Leagues')
+
+
+@application.route("/players", methods=["GET"])
+def players():
+    global db
+    games = db.getGames()
+    return render_template('list.html', items=games, pageName='Players')
+
+
+@application.route("/teams", methods=["GET"])
+def teams():
+    global db
+    games = db.getGames()
+    return render_template('list.html', items=games, pageName='Teams')
+
+
 if __name__ ==  "__main__":
     application.run(host="0.0.0.0", port=8080, debug=True)
